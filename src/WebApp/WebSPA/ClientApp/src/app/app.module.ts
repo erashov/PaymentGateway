807b9cd700aaba80c501a407fe3bbd5d3b528133
Alexandr Erashov <f0xinix@hotmail.com>
2019-03-20 18:08:09 +0300

    I've done some changes like add Dapper support and Postgres

--- src/WebApp/WebSPA/ClientApp/src/app/app.module.ts
+++ src/WebApp/WebSPA/ClientApp/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from './material/material.module'
+import { MaterialModule } from './material.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDashboardComponent } from './material-dashboard/material-dashboard.component';
-import { MyComponentComponent } from './my-component/my-component.component';
 import { RegistrationComponentComponent } from './registration-component/registration-component.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 
@@ -19,7 +18,6 @@ import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent,
     RegistrationComponentComponent,
     LoginComponentComponent,
     MaterialDashboardComponent,
