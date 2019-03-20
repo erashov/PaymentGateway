d592c51f9fa59f57fcc91457ea05201d370f414f
Alexandr Erashov <f0xinix@hotmail.com>
2019-03-20 11:18:32 +0300

    add angular 7 and I've written login and registration forms

--- src/WebApp/WebSPA/ClientApp/src/app/app.module.ts
+++ src/WebApp/WebSPA/ClientApp/src/app/app.module.ts
@@ -1,32 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
+import { MaterialModule } from './material/material.module'
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    MyComponentComponent,
+    RegistrationComponentComponent,
+    LoginComponentComponent,
+
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/WebApp/WebSPA/ClientApp/src/app/material/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatNativeDateModule, MatIconModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatListModule, } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatNativeDateModule, MatDatepickerModule, MatIconModule,
+    MatButtonModule, MatCheckboxModule, MatToolbarModule,
+    FormsModule, MatCardModule, MatFormFieldModule,
+    MatInputModule, MatListModule, MatRadioModule,
+  ],
+  exports: [MatDatepickerModule, MatIconModule, MatButtonModule,
+    MatCheckboxModule, MatToolbarModule, MatCardModule,
+    MatFormFieldModule, MatInputModule, MatListModule, MatRadioModule,],
+})
+export class MaterialModule { }
