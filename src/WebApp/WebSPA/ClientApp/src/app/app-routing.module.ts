b6302ad444957694492e8e2ee34c2e611cfccdec
Alexandr Erashov <f0xinix@hotmail.com>
2019-03-20 11:45:11 +0300

    I've added the dashboard to webspa app

--- src/WebApp/WebSPA/ClientApp/src/app/app-routing.module.ts
+++ src/WebApp/WebSPA/ClientApp/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponentComponent } from './registration-component/registration-component.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
-
-const routes: Routes = [   { path: '', redirectTo: '/', pathMatch: 'full' },
+import { MaterialDashboardComponent } from './material-dashboard/material-dashboard.component';
+const routes: Routes = [   { path: '',component: MaterialDashboardComponent },
 { path: 'register', component: RegistrationComponentComponent },
 { path: 'login', component: LoginComponentComponent },];
 
--- src/WebApp/WebSPA/ClientApp/src/app/app.module.ts
+++ src/WebApp/WebSPA/ClientApp/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { MaterialModule } from './material/material.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDashboardComponent } from './material-dashboard/material-dashboard.component';
 import { MyComponentComponent } from './my-component/my-component.component';
 import { RegistrationComponentComponent } from './registration-component/registration-component.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
+
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+import { from } from 'rxjs';
 
 
 
@@ -17,13 +22,20 @@ import { LoginComponentComponent } from './login-component/login-component.compo
     MyComponentComponent,
     RegistrationComponentComponent,
     LoginComponentComponent,
+    MaterialDashboardComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule 
+    MaterialModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
